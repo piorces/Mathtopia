ed836d3d69581562553289a2a2888e4154a811bf
Orces <pio1681@uncw.edu>
2022-09-29 09:13:11 -0400

    changing scenes and fixes

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Movement : MonoBehaviour
 {
-  
+
     public float speed;
     public Rigidbody2D rb1;
     public Vector2 movement;
@@ -18,10 +20,19 @@ public class Movement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
     void FixedUpdate()
     {
         rb1.MovePosition(rb1.position + movement * speed * Time.fixedDeltaTime);
     }
-
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Door")
+        {
+            Debug.Log("Hello");
+            SceneManager.LoadScene("Room");
+        }
+    }
 }
