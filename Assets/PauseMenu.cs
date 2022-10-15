f4b6029392ba72673eaa2dec2506c8f4f2be3888
JoshuaTWagner <joshua.t.wagner204@gmail.com>
2022-10-15 15:39:11 -0400

    added missing files woops

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pMenu;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        pMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(isPaused) {
+                ResumeGame();
+            }
+
+            else {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame() {
+        pMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame() {
+        pMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
